7c66747532c30b06bde8c3335d918ca0e920351d
Aaron Zhang <aaronbxzhang@gmail.com>
2026-07-04 21:25:14 -0400

    Remove license

--- src/usb_to_i2c.c
+++ src/usb_to_i2c.c
@@ -9,6 +9,7 @@
 #define LED_PIN 25
 #define I2C_ADDR 0x17
 #define I2C_BUS i2c1
+#define BAUDRATE 100000
 
 void init() {
     gpio_init(LED_PIN);
@@ -21,7 +22,7 @@ void init() {
 
 int main() {
     init();
-    custom_i2c_slave_init(I2C_BUS, I2C_ADDR, 100000, SDA_PIN_1, SCL_PIN_1);
+    custom_i2c_slave_init(I2C_BUS, I2C_ADDR, BAUDRATE, SDA_PIN_1, SCL_PIN_1);
 
     return 0;
 
